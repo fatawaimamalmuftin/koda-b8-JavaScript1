ef1438d892ab6b1377a1b6b422720759c2918557
XONE <muftinmarmut@gmail.com>
2026-05-13 10:34:54 +0700

    JavaCript1
    Case: menghitung Keliling dan luas lingkaran

---
+++ index.js
@@ -0,0 +1,8 @@
+let phi = 3.14; //22/7
+let Rijari = 10;
+
+const luas = phi * Rijari * Rijari;
+const keliling = 2 * phi * Rijari;
+
+console.log("Luas Lingkaran : " + luas);
+console.log("Keliling Lingkaran : " + keliling);
