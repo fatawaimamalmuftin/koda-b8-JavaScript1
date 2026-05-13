111a3a9a5f5d5f0884d35c9fd96233b1a581a1cf
XONE <muftinmarmut@gmail.com>
2026-05-13 14:17:37 +0700

    Add files via upload

---
+++ Task5.js
@@ -0,0 +1,4 @@
+const kalkulasi = { num: { first: [0, 30] } };
+const kalkulasi2 = { num2: { second: [0, 1, 2] } };
+const hasil = kalkulasi.num.first[1] + kalkulasi2.num2.second[2];
+console.log(hasil);
